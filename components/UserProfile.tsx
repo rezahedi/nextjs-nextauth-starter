d80dbcb09df2124da6ee9df6a5e4d77ae2ca7dee
Oliver Forral <intrepion@gmail.com>
2024-07-09 21:27:20 -0600

    Fixing React Invalid DOM warnings.

--- components/UserProfile.tsx
+++ components/UserProfile.tsx
@@ -28,9 +28,9 @@ export default function UserProfile() {
           xmlns="http://www.w3.org/2000/svg"
         >
           <path
-            fill-rule="evenodd"
+            fillRule="evenodd"
             d="M10 9a3 3 0 100-6 3 3 0 000 6zm-7 9a7 7 0 1114 0H3z"
-            clip-rule="evenodd"
+            clipRule="evenodd"
           ></path>
         </svg>
         Sign In
